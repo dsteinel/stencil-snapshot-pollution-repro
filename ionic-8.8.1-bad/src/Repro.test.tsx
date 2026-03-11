08a3c63dd1c593b6526afd00f7387c3abd66e943
Dimitri Steinel <dimitri.steinel@bsdex.de>
2026-03-11 11:12:55 +0100

    test: 💍 add snapshot tests

--- ionic-8.8.1-bad/src/Repro.test.tsx
+++ ionic-8.8.1-bad/src/Repro.test.tsx
@@ -6,7 +6,11 @@ import { describe, expect, it } from 'vitest'
 describe('Stencil Expando Repro', () => {
   it('should verify toHaveTextContent behavior', () => {
     const { container } = render(<IonButton>Test</IonButton>)
+    console.log('HTML in 8.8.1:', container.innerHTML)
     const button = container.querySelector('ion-button')
+
+    expect(button).toMatchSnapshot()
     expect(button).toHaveTextContent('Test')
+    expect(button).toBeVisible()
   })
 })
